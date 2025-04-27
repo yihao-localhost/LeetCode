59815b9382b88d52125e657605d38add568cfd09
yihao-localhost <yihao_localhost@163.com>
2025-04-27 11:45:49 +0800

    250427，完成了三道题

--- src/Main_250427_03.java
+++ src/Main_250427_03.java
@@ -7,8 +7,7 @@ public class Main_250427_03 {
     public List<String> summaryRanges(int[] nums) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(nums[i]);
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(nums[i]));
             if (i != nums.length - 1 && nums[i + 1] - 1 == nums[i]) {
                 while (nums[i + 1] - 1 == nums[i]) {
                     i++;
